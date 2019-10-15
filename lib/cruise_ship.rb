3e8840fae5ed50995075f66a7b7282875343c551
Andrew Capp <acapp909@gmail.com>
2019-10-15 17:51:51 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -10,8 +10,8 @@ require 'pry'
 def select_winner(passengers)
   # add the code snippet here!
   passengers.each do |key, value|
-    #binding.pry
-    if :suite_a == value.start_with?("A")
+    binding.pry
+    if key == :suite_a && value.start_with?("A")
       puts "Winner is: #{value}!"
     end
   end
