65bc15e2fab51c14a3b0f068de776350ec6eac7f
Andrew Capp <acapp909@gmail.com>
2019-10-15 17:43:03 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -1,16 +1,20 @@
-# passengers = {
-# suite_a: "Amanda Presley",
-# suite_b: "Seymour Hoffman",
-# suite_c: "Alfred Tennyson",
-# suite_d: "Charlie Chaplin",
-# suite_e: "Crumpet the Elf"
-# }
+require 'pry'
+ passengers = {
+ suite_a: "Amanda Presley",
+ suite_b: "Seymour Hoffman", 
+ suite_c: "Alfred Tennyson",
+ suite_d: "Charlie Chaplin",
+ suite_e: "Crumpet the Elf"
+ }
 
 def select_winner(passengers)
   # add the code snippet here!
   passengers.each do |key, value|
+    binding.pry 
     if key[:suite_a] == value.start_with?("A")
       puts "Winner is: #{value]!"
     end
 
 end
+
+select_winner(passengers)
