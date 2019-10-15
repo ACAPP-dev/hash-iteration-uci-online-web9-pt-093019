61dde35234a7f66d96f8c765769074f06955d696
Andrew Capp <acapp909@gmail.com>
2019-10-15 18:05:24 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -9,7 +9,7 @@ def happy_birthday(birthday_kids)
   birthday_kids.each do |name, age|
 
     puts "Happy Birthday #{name}! You are now #{age} years old!"
-  
+
   end
 end
 
@@ -18,6 +18,8 @@ def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |name, age|
     if age < 12
     puts "Happy Birthday #{name}! You are now #{age} years old!"
+  else
+    puts "You are too old for this."
   end
   end
 end
